c0af204c68724cb976beb9838a9377d8122dd9a9
Chad Carbert <chadcarbert@me.com>
2018-06-11 19:17:38 +0200

    Add IndexedDB storage adapter and add storePhoto to fetcher

---
+++ src/utils/lib/fetcher.js
@@ -0,0 +1,50 @@
+import Flickr from "flickr-sdk/flickr-sdk";
+import env from "../../../config/environment";
+import { take, clone } from "ramda";
+import shuffle from './shuffle';
+import { SimpleIndexedDbAdapter } from "./storage";
+
+const flickr = new Flickr(env.FLICKR_API_KEY);
+
+const highQualityImageUrlForPhoto = photo => photo.url_o || photo.url_l;
+let indexStorage = new SimpleIndexedDbAdapter('VORFREUDE_PHOTO_STORAGE');
+
+function fetchPhotos(photos) {
+  photos = clone(photos);
+
+  return Promise.all(
+    photos.map(photo => {
+      let url = highQualityImageUrlForPhoto(photo);
+      return fetch(url)
+        .then(response => response.blob())
+        .then(blob => ((photo.blob = blob), photo));
+    })
+  );
+}
+
+function storePhoto(photo) {
+  return indexStorage.set(photo.id, photo);
+}
+
+export async function query(searchTerms) {
+  let result = await flickr.photos.search({
+    safe_search: "1",
+    text: searchTerms,
+    privacy_filter: "1",
+    sort: "interestingness-desc",
+    per_page: "500",
+    extras: "url_o"
+  });
+
+  let photos = result.body.photos.photo || [];
+  return photos
+    .filter(highQualityImageUrlForPhoto)
+    .map(photo => (photo.searchTerms = searchTerms, photo));
+};
+
+export async function fetchPopularPhotoUrl(searchTerm) {
+  let photos = await query(searchTerm)
+  let photoUrls = photos.map(highQualityImageUrlForPhoto);
+
+  return take(50, shuffle(photoUrls)).pop();
+}
--- src/utils/lib/storage.ts
+++ src/utils/lib/storage.ts
@@ -3,17 +3,22 @@ interface StorageAdapter {
   set(key:String, value:any):Promise<any>;
 }
 
-export const getEnvironmentStorage = () => chrome.storage ?
-    new ChromeStorageAdapter()
-  : new LocalStorageAdapter();
+export const getEnvironmentStorage = (storeName) => chrome.storage ?
+    new ChromeStorageAdapter(storeName)
+  : new LocalStorageAdapter(storeName);
 
 export class LocalStorageAdapter implements StorageAdapter {
+  constructor(storeName) {
+    this.storeName = storeName;
+  }
+
+  storeName = ""
+
   get(key) {
     return new Promise((resolve, reject) => {
       try {
-        resolve(
-          JSON.parse(window.localStorage.getItem(key))
-        );
+        let store = JSON.parse(window.localStorage.getItem(this.storeName));
+        resolve((store && store[key]) || null);
       } catch (e) {
         reject(e);
       }
@@ -23,7 +28,8 @@ export class LocalStorageAdapter implements StorageAdapter {
   set(key, value) {
     return new Promise((resolve, reject) => {
       try {
-        window.localStorage.setItem(key, JSON.stringify(value));
+        let store = JSON.parse(window.localStorage.getItem(this.storeName));
+        window.localStorage.setItem(this.storeName, JSON.stringify({ ...store, [key]: value }));
         resolve(value);
       } catch (e) {
         reject(e);
@@ -33,19 +39,89 @@ export class LocalStorageAdapter implements StorageAdapter {
 }
 
 export class ChromeStorageAdapter implements StorageAdapter {
+  constructor(storeName) {
+    this.storeName = storeName;
+  }
+
+  storeName = ""
+
   get(key) {
     return new Promise((resolve, reject) => {
-      chrome.storage.local.get(key, result => {
-        resolve(result[key]);
+      chrome.storage.local.get(this.storeName, result => {
+        resolve((result && result[key]) || null);
       });
     });
   }
 
   set(key, value) {
     return new Promise((resolve, reject) => {
-      chrome.storage.local.set({ [key] : value }, (result) => {
-        result ? resolve(value) : undefined;
+      chrome.storage.local.get(this.storeName, (store) => {
+        store = store || {};
+        chrome.storage.local.set({ [this.storeName] : { ...store, [key]: value } }, (result) => {
+          result ? resolve(value) : undefined;
+        });
       });
     });
+  }
+}
+
+export class SimpleIndexedDbAdapter implements StorageAdapter {
+  constructor(tableName) {
+    this.tableName = tableName;
+
+    let request = indexedDB.open(SimpleIndexedDbAdapter.DB_KEY);
+
+    this.db = new Promise((resolve, reject) => {
+      request.onsuccess = () => {
+        let db = request.result;
+        resolve(db);
+      };
+
+      request.onerror = reject;
+    });
+
+    request.onupgradeneeded = () => {
+      this.table = request.result.createObjectStore(tableName, { keyPath: "__id" });
+    };
+  }
+
+  static DB_KEY = '__VORFREUDE'
+
+  db = null
+  table = null
+  tableName = ""
+
+  get(key) {
+    let dbRead = db => new Promise((resolve, reject) => {
+      let transaction = db.transaction(this.tableName, "readwrite");
+
+      transaction.onerror = reject;
+      transaction.onabort = reject;
+      transaction.oncomplete = event => {
+        resolve(request.result);
+      };
+
+      let request = transaction.objectStore(this.tableName).get(key);
+    });
+
+    return this.db.then(db => dbRead(db));
+  }
+
+  set(key, value) {
+    let savedFormat = { __id: key, ...value };
+
+    let dbWrite = db => new Promise((resolve, reject) => {
+      let transaction = db.transaction(this.tableName, 'readwrite');
+
+      transaction.onerror = reject;
+      transaction.onabort = reject;
+      transaction.oncomplete = event => resolve(key);
+
+      transaction.objectStore(this.tableName).put(savedFormat);
+    });
+
+    return this.db
+      .then(db => dbWrite(db))
+      .then(() => savedFormat);
   }
 }

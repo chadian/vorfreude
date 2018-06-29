f852ca86fd92693d03aada3fb7b818b1cf6125c7
Chad Carbert <chadcarbert@me.com>
2018-06-29 08:08:35 +0200

    Clean up environment config

--- config/environment.js
+++ config/environment.js
@@ -4,12 +4,8 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'vorfreude',
     IMAGE_ENDPOINT_URL: 'https://vorfreude.now.sh/',
-    environment,
+    environment
   };
-
-  if (environment === 'production') {
-    ENV.IMAGE_ENDPOINT_URL = "https://vorfreude-server-ypttjqnzcu.now.sh/";
-  }
 
   return ENV;
 };

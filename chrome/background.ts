d2e1a7175cd10c9731de0ae7edbdaa16ffed43f6
Chad Carbert <chadcarbert@me.com>
2018-07-02 23:16:40 +0200

    Open new tab with chrome browser action icon

--- chrome/background.ts
+++ chrome/background.ts
@@ -1,10 +1,16 @@
-declare const chrome: { runtime };
-import { replenish } from "../src/utils/lib/fetcher";
+import { replenish } from '../src/utils/lib/fetcher';
 
 chrome.runtime.onMessage.addListener(function (message, callback) {
-  if (message.operation == "replenishBacklog") {
+  if (message.operation == 'replenishBacklog') {
     let { searchTerms, downloadBatchSize } = message;
 
     return replenish(searchTerms);
   }
 });
+
+function openVorfreudeTab() {
+    let vorfreudeUrl = chrome.extension.getURL('window.html');
+    chrome.tabs.create({ url: vorfreudeUrl });
+}
+
+chrome.browserAction.onClicked.addListener(() => openVorfreudeTab());

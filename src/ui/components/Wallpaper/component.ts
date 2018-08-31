ee8a339d984d8e7896126281da7508705cc5e5ca
Chad Carbert <chadcarbert@me.com>
2018-08-31 22:23:17 +0200

    Improve background image performance and clean up setting image

--- src/ui/components/Wallpaper/component.ts
+++ src/ui/components/Wallpaper/component.ts
@@ -12,17 +12,9 @@ export default class Wallpaper extends Component {
     } = this.args;
 
     const wallpaperElement = this.bounds.firstNode as HTMLElement;
-    let nextImage = `url(${ photoUrl })`;
 
     requestAnimationFrame(() => {
-      let currentImage = wallpaperElement.style.backgroundImage.split(',').reverse().pop();
-
-      // layer the current background image with the existing
-      // to avoid nasty flashes.
-      let backgroundImage = [nextImage, currentImage]
-        .filter(Boolean).join(',');
-
-      wallpaperElement.style.backgroundImage = backgroundImage;
+      wallpaperElement.style.backgroundImage = `url(${photoUrl})`;
 
       if (shouldBlur === true) {
         wallpaperElement.classList.add('Wallpaper__blurred');

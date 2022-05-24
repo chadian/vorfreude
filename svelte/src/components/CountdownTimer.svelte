fa34120caff8aefb18277b65365347579ac23c29
Chad Carbert <chadcarbert@me.com>
2022-05-24 11:05:28 -0400

    Relocate global styles and fonts

--- svelte/src/components/CountdownTimer.svelte
+++ svelte/src/components/CountdownTimer.svelte
@@ -1,32 +1,64 @@
 <script>
-  export let time;
-  import plural from '../helpers/plural-rules';
+	export let time;
+	import plural from '../helpers/plural-rules';
 </script>
 
 <div class="CountdownTimer">
-  {#if time}
-    {#if time.days}
-      <div class="CountdownTimer__segment">
-        {time.days} {plural(time.days, 'days')}
-      </div>
-    {/if}
-  
-    {#if time.hours}
-      <div class="CountdownTimer__segment">
-        {time.hours} {plural(time.hours, 'hours')}
-      </div>
-    {/if}
+	{#if time}
+		{#if time.days}
+			<div class="CountdownTimer__segment">
+				{time.days}
+				{plural(time.days, 'days')}
+			</div>
+		{/if}
 
-    {#if time.minutes}
-      <div class="CountdownTimer__segment">
-        {time.minutes} {plural(time.minutes, 'minutes')}
-      </div>
-    {/if}
+		{#if time.hours}
+			<div class="CountdownTimer__segment">
+				{time.hours}
+				{plural(time.hours, 'hours')}
+			</div>
+		{/if}
 
-    {#if time.seconds}
-      <div class="CountdownTimer__segment">
-          {time.seconds} {plural(time.seconds, 'seconds')}
-      </div>
-    {/if}
-  {/if}
+		{#if time.minutes}
+			<div class="CountdownTimer__segment">
+				{time.minutes}
+				{plural(time.minutes, 'minutes')}
+			</div>
+		{/if}
+
+		{#if time.seconds}
+			<div class="CountdownTimer__segment">
+				{time.seconds}
+				{plural(time.seconds, 'seconds')}
+			</div>
+		{/if}
+	{/if}
 </div>
+
+<style>
+	.CountdownTimer {
+		font-size: calc(var(--base-screen-proportional-font-size-desktop) * 0.8);
+	}
+
+	@media only screen and (max-width: 425px) {
+		.CountdownTimer {
+			font-size: calc(var(--base-screen-proportional-font-size-mobile) * 0.85);
+		}
+	}
+
+	.CountdownTimer__segment {
+		display: inline-block;
+	}
+
+	@media only screen and (max-width: 425px) {
+		.CountdownTimer__segment {
+			display: block;
+		}
+	}
+
+	@media only screen and (max-width: 425px) {
+		.CountdownTimer__segment {
+			display: block;
+		}
+	}
+</style>
--- svelte/src/routes/index.svelte
+++ svelte/src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script>
-  import CountdownTimer from '../components/CountdownTimer.svelte';
+	import CountdownTimer from '../components/CountdownTimer.svelte';
+  import '../global.css';
 </script>
 
 <CountdownTimer time={{ days: '1', hours: '1', minutes: '1', seconds: '1' }} />

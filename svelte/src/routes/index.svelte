673ca5dc293aa28e7f713d61296e97ecf6715872
Chad Carbert <chadcarbert@me.com>
2022-05-27 16:33:34 -0400

    Add "shell" wrapper to provide centering and base structure

--- svelte/src/routes/index.svelte
+++ svelte/src/routes/index.svelte
@@ -1,7 +1,37 @@
 <script>
 	import CountdownTimer from '../components/CountdownTimer.svelte';
-  import Settings from '../components/Settings.svelte';
-  import '../global.css';
+	import Settings from '../components/Settings.svelte';
+	import '../global.css';
 </script>
 
-<Settings />
+<div class="shell">
+	<Settings />
+</div>
+
+<style>
+	@keyframes show {
+		from {
+			opacity: 0;
+		}
+
+		to {
+			opacity: 1;
+		}
+	}
+
+	.shell {
+		width: 100%;
+		height: 100%;
+		margin: 0;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		opacity: 0;
+		animation-name: show;
+		animation-delay: 300ms;
+		animation-duration: 500ms;
+		animation-fill-mode: forwards;
+		animation-timing-function: cubic-bezier(0.175, 0, 4, 0.885);
+	}
+</style>

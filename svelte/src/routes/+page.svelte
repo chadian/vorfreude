19ec760a275bfd77a293ae32275d1f6913f225aa
Chad Carbert <ccarbert@salesforce.com>
2022-08-18 15:58:25 -0400

    Add SettingsButton with gear icon

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,13 +1,40 @@
 <script>
   import CountdownTimer from '../components/CountdownTimer.svelte';
   import CountdownMessage from '../components/CountdownMessage.svelte';
+  import SettingsButton from '../components/SettingsButton.svelte';
   import { getSettingsStore } from '../state/stores/settings';
   const settingsStore = getSettingsStore();
+
+  function goToSettings() {
+    document.location = '/settings';
+  }
 </script>
+
+<div class="Index__SettingsButton">
+  <SettingsButton onClick={goToSettings}/>
+</div>
 
 <CountdownMessage
   endDate={$settingsStore.date}
   countdownMessage={$settingsStore.countdownMessage}
   allDoneMessage={$settingsStore.allDoneMessage}
 />
+
 <CountdownTimer endDate={$settingsStore.date}/>
+
+<style>
+  .Index__SettingsButton {
+    /* INITIAL OPACITY */
+    opacity: 0.75;
+
+    display: block;
+    position: fixed;
+    top: 1.2rem;
+    right: 1.2rem;
+  }
+
+  .Index__SettingsButton:hover {
+    /* HOVER OPACITY */
+    opacity: 1;
+  }
+</style>

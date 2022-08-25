4cbc11a56bc39ea36d42a6a1e6b22ba2f730bc17
Chad Carbert <ccarbert@salesforce.com>
2022-08-24 21:26:15 -0400

    Add basic acceptance test

--- svelte/tests/acceptance/test.js
+++ svelte/tests/acceptance/test.js
@@ -1,5 +1,12 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
+test('it shows the default state', async ({ page }) => {
+  await page.goto('/');
+  const message = await page.locator('.Vorfreude__countdown-message');
+  expect(await message.textContent()).toContain('Vorfreude');
+
+  const timer = await page.locator('.CountdownTimer');
+
+  // :shrug: this might fail if this test is ran on the same day as the default date
+  expect(await timer.textContent()).toMatch(/\d+ days \d+ hours \d+ minutes \d+ seconds/)
 });

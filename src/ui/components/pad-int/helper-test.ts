52d0630fdd448cfc9c2cb4405dc31d373c0f63f4
Chad Carbert <chadcarbert@me.com>
2022-09-05 22:19:44 -0400

    Remove of all the ember things

--- src/ui/components/pad-int/helper-test.ts
+++
@@ -1,9 +0,0 @@
-import padInt from './helper';
-
-const { module, test } = QUnit;
-
-module('Helper: pad-int', function(hooks) {
-  test('it computes', function(assert) {
-    assert.expect(0);
-  });
-});
--- src/ui/components/pad-int/helper.ts
+++
@@ -1,6 +0,0 @@
-export default function padInt([num, digits]: [any, number]): string {
-  let numberString = Number(num).toString();
-  let fillAmount = digits - numberString.length;
-  let pad = fillAmount > 0 ? Array(fillAmount).fill('0').join('') : '';
-  return pad + numberString;
-}

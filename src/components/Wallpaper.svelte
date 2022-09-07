bbbc0cdbc995c75d43b186710dd3bff57b1c3ee0
Chad Carbert <chadcarbert@me.com>
2022-09-06 20:46:43 -0400

    Tweak Wallpaper component
    * Ensure image is fully loaded before showing wallpaper
    * Use vw and vh units instead of 100% for width and height
    * Use object-fit with img tag instead of background-image with div

--- src/components/Wallpaper.svelte
+++ src/components/Wallpaper.svelte
@@ -5,36 +5,52 @@
   const baseClass = 'Wallpaper__img';
   const blurClass = 'Wallpaper__blurred-img';
 
+  $: loaded = false;
   $: imageClasses = blur ? [baseClass, blurClass] : [baseClass];
+
+  $: {
+    if (photoUrl) {
+      const img = document.createElement('img');
+
+      img.onload = () => {
+        loaded = true;
+      }
+
+      img.src = photoUrl;
+    }
+  }
 </script>
 
-<div class="Wallpaper">
-  <div style="background-image: url({photoUrl})" class={imageClasses.join(' ')}></div>
-</div>
+{#if loaded}
+  <div class="Wallpaper">
+    <img src={photoUrl} class={imageClasses.join(' ')} />
+  </div>
+{/if}
 
 <style>
   .Wallpaper {
-    width: 100%;
-    height: 100%;
+    width: 100vw;
+    height: 100vh;
     margin: 0;
     z-index: -1;
     position: fixed;
-    zoom: 1.1;
+    top: 0;
+    left: 0;
   }
 
   .Wallpaper__img {
     position: absolute;
     top: 0;
-    left: 0;
     right: 0;
     bottom: 0;
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    object-fit: cover;
+    object-position: center center;
     transition-duration: 375ms, 325ms;
     transition-property: transform, filter;
     transition-timing-function: cubic-bezier(0.35, 0.25, 0.21, 0.96);
-    transform: scale(1.02);
     will-change: transform, filter;
   }
 

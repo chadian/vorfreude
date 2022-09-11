9a461fd6a0d79e240a1631496acbdb6b796a9ce0
Chad Carbert <chadcarbert@me.com>
2022-09-10 22:04:59 -0400

    Add hacky index.html route
    This commit adds a route that is as an ugly hack for when
    "index.html" loads and it tries to load the corresponding route.
    Without this "index.html" route the router can't find a matching
    route, throws an error, but still ends up at the default route
    (so the user doesn't notice the issue) but it leaves an ugly
    error in the console.
    
    By having an "index.html" route which is a shortcoming of being
    able to serve from static files like within extensions, this
    route kicks things off by navigating to the root route "/".

---
+++ src/routes/index.html/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  /*
+    This page serves as an ugly hack for when "index.html" loads
+    and it tries to load the corresponding route, that ends up
+    being this, and redirecting back to the root kicks off
+    the whole initial flow right. Without this file, the router
+    can't find this route, throws an error, but still ends up
+    at the default route.
+  */
+
+  import { goto } from "$app/navigation";
+  export const prerender = false;
+  export const ssr = false;
+  goto('/');
+</script>
